5680edf56e57129323e848e4c9133ffe04d9733f
voltelxu <voltelxu@gmain.com>
2017-12-19 15:36:34 +0800

    lstm seg change

--- raw_pre/raw_lstm.py
+++ raw_pre/raw_lstm.py
@@ -65,6 +65,13 @@ def getdata(filename):
 	len_data = len(datas)
 	#shuffle the data set
 	random.shuffle(datas)
+	# datas length sentence
+	dataslen = [length for length in [len(x[0]) for x in datas]]
+	# padding data
+	for d in datas:
+		l = len(d[0])
+		d[0] += [0 for i in range(max_scelen - l)]
+		d[1] += [0 for i in range(max_scelen - l)]
 	#split position
 	split = int(len_data * train_rate)
 	#split dataset to train and test sets
@@ -74,55 +81,62 @@ def getdata(filename):
 	train_data = [t for t in [x[0] for x in train]]
 	#train_lable
 	train_lable = [t for t in [x[1] for x in train]]
+	# train lenght
+	train_len = dataslen[0 : split]
 	#test_data
 	test_data = [t for t in [x[0] for x in test]]
 	#test_lable
 	test_lable = [t for t in [x[1] for x in test]]
+	# test_data length0
+	test_len = dataslen[split : len_data]
 	#return train and test set
-	return train_data, train_lable, test_data, test_lable, max_scelen
+	return train_data, train_lable, train_len, test_data, test_lable, test_len, max_scelen
 
-train_data,train_lable, test_data, test_lable, max_len = getdata("test")
+train_data,train_lable, train_len, test_data, test_lable, test_len, max_len = getdata("test")
 # max sequence length 
 max_num_step = max_len
 
 # get batch input data
-def getbatch(data, lable, start, batch_size):
+def getbatch(data, lable, lens, start, batch_size):
 	batch_data = data[start : start + batch_size]
-	batch_seqlen = [length for length in [len(x) for x in batch_data]]
-
-	batch_datas = list()
-	for d in batch_data:
-		l = len(d)
-		d += [0 for i in range(max_num_step - l)]
-		batch_datas.append(d)
-
+	batch_seqlen = lens[start : start + batch_size]
 	batch_lable = lable[start : start + batch_size]
-	batch_lables = list()
-	for b in batch_lable:
-		l = len(b)
-		b += [0 for i in range(max_num_step - l)]
-		batch_lables.append(b)
-	
-	return batch_datas, batch_lables, batch_seqlen
+	return batch_data, batch_lable, batch_seqlen
 
 # batch_data, batch_lable, batch_seqlen = getbatch(train_data, train_lable, 1, 8)
 # print len(batch_data)
 
-def dynamicRNN(inputs, seqlen, weights):
+def dynamicRNN(inputs, seqlen, weights, bias):
 	#
-	inputs = tf.unstack(inputs, max_num_step, 1)
+	# inputs = tf.unstack(inputs, max_num_step, 1)
 	#lstm cell
 	lstm_cell = tf.contrib.rnn.BasicLSTMCell(hidden_size)
+	#dropout
+	# cell = tf.contrib.rnn.DropoutWrapper(lstm_cell, output_keep_prob=0.7)
 	#outputs and state
-	outputs, state = tf.contrib.rnn.static_rnn(lstm_cell, inputs, dtype=tf.float32, 
-		sequence_length=seqlen)
+	# outputs, state = tf.contrib.rnn.static_rnn(cell, inputs, dtype=tf.float32, 
+		# sequence_length=seqlen)
+	outputs, state = tf.nn.dynamic_rnn(lstm_cell, inputs, dtype=tf.float32, sequence_length=seqlen)
+	
 	# outputs shape [num_step, batch_size, hidden_size]
-	outputs = tf.stack(outputs)
-	# outputs shape [batch_size, num_step, hidden_size]
-	outputs = tf.transpose(outputs, [1, 0, 2])
+	# outputs = tf.stack(outputs)
 
-	outputs = tf.reshape(outputs, [-1, hidden_size])
-	return tf.matmul(outputs, weights)
+	#last out puts
+	'''batch = tf.shape(outputs)[0]
+	outsize = tf.shape(outputs)[2]
+	index = [tf.range(0, batch),(seqlen - 1)]
+	outputs = tf.gather(tf.reshape(outputs, [-1, outsize]), index)'''
+
+	outputs = tf.reshape(outputs, [batch_size * max_num_step, hidden_size])
+	#outputs shape [batch_size, num_step, hidden_size]
+
+	outputs = tf.matmul(outputs, weights) + bias
+
+	# outputs = tf.reshape(outputs, [batch_size, max_num_step, lable_size])
+
+	# softmax = tf.nn.softmax(outputs)
+	# softmax = tf.reshape(softmax, [batch_size, max_num_step, lable_size])
+	return outputs
 
 def model():
 	# input data
@@ -131,24 +145,42 @@ def model():
 	target = tf.placeholder(dtype=tf.int32, shape=[batch_size, max_num_step])
 	# true seq lenght
 	seqlen = tf.placeholder(dtype=tf.int32, shape=[None])
+	# seq mask [1, 0]
+	# selmask = tf.sequence_mask(seqlen, maxlen=max_num_step, dtype=tf.float32)
+	seqem = tf.constant(np.tril(np.ones([max_num_step + 1, max_num_step]), -1), dtype=tf.float32)
+	seqmask = tf.nn.embedding_lookup(seqem, seqlen)
 	# embeddings
 	embedding = tf.get_variable(name='embedding', shape=[voc_size, embed_size])
 	# change word to tensor
 	inputs = tf.nn.embedding_lookup(embedding, input_data)
 	# weights 
 	weights = tf.get_variable("weights",[hidden_size, lable_size])
+	# bias
+	bias = tf.get_variable("bias", [lable_size])
 	# rnn cell
-	pred = dynamicRNN(inputs, seqlen, weights)
+	logits = dynamicRNN(inputs, seqlen, weights, bias)
+	#
+	pred = tf.nn.softmax(logits)
+	pred = tf.cast(tf.argmax(pred, 1), tf.int32)
+	# out = tf.argmax(pred,1)
+	correct = tf.cast(tf.equal(pred, tf.reshape(target, [-1])), tf.int32) * tf.cast(tf.reshape(seqmask, [-1]), tf.int32)
+	# accuracy
+	accuracy = tf.reduce_sum(tf.cast(correct, tf.float32))/tf.reduce_sum(tf.cast(seqmask, tf.float32))
+	#
+	loss = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=tf.reshape(target, [-1]))
+	loss = loss * tf.reshape(seqmask, [-1])
+	cost = tf.reduce_sum(loss) / tf.reduce_sum(seqmask)
+	#optimizer
+	opt = tf.train.AdamOptimizer(0.1).minimize(cost)
 	# 
-	logits = tf.reshape(pred, [batch_size, max_num_step, lable_size])
+	'''logits = tf.reshape(pred, [batch_size, max_num_step, lable_size])
 
 	loss = tf.contrib.seq2seq.sequence_loss(logits,
 	 			target, 
 	 			tf.ones([batch_size, max_num_step]))
-	tf.summary.scalar("loss", loss)
 	cost = tf.reduce_mean(loss)
 
-	softmax_out = tf.nn.softmax(tf.reshape(logits, [-1, lable_size]))
+	softmax_out = tf.nn.softmax(tf.reshape(logits, [-1, lable_size]))0
 	predict = tf.cast(tf.argmax(softmax_out, axis=1),tf.int32)
 	correct = tf.equal(predict, tf.reshape(target,[-1]))
 	accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
@@ -157,7 +189,7 @@ def model():
 	tvars = tf.trainable_variables()
 	grads, _ = tf.clip_by_global_norm(tf.gradients(cost, tvars), 5)
 	opt = tf.train.GradientDescentOptimizer(learning_rate=lr).apply_gradients(zip(grads,tvars))
-
+'''
 	init = tf.global_variables_initializer()
 
 	with tf.Session() as sess:
@@ -167,20 +199,28 @@ def model():
 		# train
 		epoch_size = len(train_data)/batch_size
 		testsize = len(test_data)/batch_size
+		'''
+		for s in range(20):
+			alls = 0.0
+			for i in range(epoch_size):
+				x, y, l = getbatch(train_data, train_lable, train_len, i * batch_size, batch_size)
+				_, a, c = sess.run([opt, accuracy, correct], feed_dict={input_data : x, target : y, seqlen : l})
+				alls += a
+			print alls/epoch_size
+		'''
 		for step in range(20000):
 			for i in xrange(0, epoch_size, 1):
-				x, y, l = getbatch(train_data, train_lable, i * batch_size, batch_size)
+				x, y, l = getbatch(train_data, train_lable, train_len, i * batch_size, batch_size)
 				sess.run([opt], feed_dict={input_data : x, target : y, seqlen : l})
 			sum_loss = 0.0
 			sum_ac = 0.0
 			num = 0
 			for j in xrange(0, testsize, 1):
 				num = num + 1
-				tx, ty, tl = getbatch(test_data, test_lable, j * batch_size, batch_size)
-				summary, ac, loss = sess.run([merged, accuracy, cost], feed_dict={input_data : tx, target : ty, seqlen : tl})
+				tx, ty, tl = getbatch(test_data, test_lable, test_len, j * batch_size, batch_size)
+				ac, loss = sess.run([accuracy, cost], feed_dict={input_data : tx, target : ty, seqlen : tl})
 				sum_ac = sum_ac + ac
 				sum_loss = sum_loss + loss
 			sum_ac = sum_ac/num
-			if step % 100 == 0:
-				print "step "+str(step)+", accuracy "+str(sum_ac)+", loss " + str(sum_loss)
+			print "step "+str(step)+", accuracy "+str(sum_ac)+", loss " + str(sum_loss)
 model()
